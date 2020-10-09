44ee208e949b513d56d8fec15e93f13fef6a14ee
Giber05 <72600935+Giber05@users.noreply.github.com>
2020-10-09 15:51:48 +0700

    Add files via upload

---
+++ JavaSample.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasample;
+
+/**
+ *
+ * @author Asus
+ */
+public class JavaSample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        System.out.println("We will not use'Hello World!");
+        System.out.println();
+        System.out.println("Now i am learning about java!");
+        
+    }
+    
+}
