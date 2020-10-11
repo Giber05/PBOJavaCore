48f10aa57394b6cbbf4ab426685fee559d3232a1
Giber05 <72600935+Giber05@users.noreply.github.com>
2020-10-11 23:53:25 +0700

    Add files via upload

---
+++ substrings.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasample;
+
+/**
+ *
+ * @author Asus
+ */
+public class substrings 
+{
+    public static void main(String[]args)
+    {
+        String sentence = "To Infinity and Beyond";
+        String word = sentence.substring(0,11);
+        
+        System.out.println(sentence);
+        System.out.println(word);
+    }
+    
+}
